200f10d3fe316bd9a52595b3e241ac4883f628dd
Sean Sundberg <seansund@us.ibm.com>
2022-03-01 17:03:36 -0600

    Updates oc-login to use external data source (#10)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/oc-login.sh
+++ scripts/oc-login.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
-SERVER="$1"
+INPUT=$(tee)
+
+SERVER=$(echo "${INPUT}" | grep "serverUrl" | sed -E 's/.*"serverUrl": ?"([^"]*)".*/\1/g')
+USERNAME=$(echo "${INPUT}" | grep "username" | sed -E 's/.*"username": ?"([^"]*)".*/\1/g')
+PASSWORD=$(echo "${INPUT}" | grep "password" | sed -E 's/.*"password": ?"([^"]*)".*/\1/g')
+TOKEN=$(echo "${INPUT}" | grep "token" | sed -E 's/.*"token": ?"([^"]*)".*/\1/g')
+KUBE_CONFIG=$(echo "${INPUT}" | grep "kube_config" | sed -E 's/.*"kube_config": ?"([^"]*)".*/\1/g')
+TMP_DIR=$(echo "${INPUT}" | grep "tmp_dir" | sed -E 's/.*"tmp_dir": ?"([^"]*)".*/\1/g')
+
+if [[ -z "${TMP_DIR}" ]]; then
+  TMP_DIR="${PWD}/.tmp/cluster"
+fi
+mkdir -p "${TMP_DIR}"
+
+if [[ -z "${KUBE_CONFIG}" ]]; then
+  KUBE_CONFIG="${TMP_DIR}/.kube/config"
+fi
 
 if [[ -z "${USERNAME}" ]] && [[ -z "${PASSWORD}" ]] && [[ -z "${TOKEN}" ]]; then
-  echo "The username and password or the token must be provided to log into ocp"
+  echo '{"message": "The username and password or the token must be provided to log into ocp"}'
   exit 1
 fi
 
@@ -13,5 +29,10 @@ else
   AUTH="--username=${USERNAME} --password=${PASSWORD}"
 fi
 
-echo "Logging in to OCP cluster: ${SERVER}"
-oc login --insecure-skip-tls-verify=true ${AUTH} --server="${SERVER}" > /dev/null
+if oc login --insecure-skip-tls-verify=true ${AUTH} --server="${SERVER}" 1> /dev/null 2> "${TMP_DIR}/error.log"; then
+  echo "{\"status\": \"success\", \"message\": \"success\", \"kube_config\": \"${KUBE_CONFIG}\"}"
+  exit 0
+else
+  echo "{\"status\": \"error\", \"message\": \"$(cat ${TMP_DIR}/error.log)\"}"
+  exit 1
+fi
